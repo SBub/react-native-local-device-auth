dfe9e42d0aff4fc21e533cf95754f71de9a94638
sbub <svetaibuben@gmail.com>
2020-08-12 01:54:18 +0200

    Basic setup for npm package

---
+++ example/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Test from 'react-native-local-device-auth';
+
+const App: () => React$Node = () => {
+  return <Test />;
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Test = () => (
+  <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+    <Text>Hello!!!</Text>
+  </View>
+);
+
+export default Test;
